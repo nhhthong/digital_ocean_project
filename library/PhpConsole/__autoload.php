5ce307d9f1f7b94cf6a5dd2aef32c9799edf3ecb
nguyenhunghuythong <huythong.nguyen@oppo-aed.vn>
2023-08-19 15:18:37 +0700

    update

--- library/PhpConsole/__autoload.php
+++ library/PhpConsole/__autoload.php
@@ -5,6 +5,6 @@ namespace PhpConsole;
 spl_autoload_register(function ($class) {
 	if(strpos($class, __NAMESPACE__) === 0) {
 		/** @noinspection PhpIncludeInspection */
-		require_once(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php');
+		require_once(__DIR__ . '/' . '..' . '/' . str_replace('\\', '/', $class) . '.php');
 	}
 });
