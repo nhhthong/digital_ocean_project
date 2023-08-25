7817859e66ea60d4573a3020e178461b2a8a7239
nguyenhunghuythong <huythong.nguyen@oppo-aed.vn>
2023-08-25 09:45:18 +0700

    upload

--- application/controllers/staff/create.php
+++ application/controllers/staff/create.php
@@ -1,19 +1,24 @@
 <?php
-$id     = $this->getRequest()->getParam('id');
-$QTeam  = new Application_Model_Team();
-$QProvince = new Application_Model_Province();
-$recursiveDeparmentTeamTitle = $QTeam->get_recursive_cache();
-$this->view->recursiveDeparmentTeamTitle = $recursiveDeparmentTeamTitle;
-$provinces = $QProvince->get_all2(null, null, ID_PROVINCE_CITIZEN_IDENTITY_CARD);
-$this->view->provinces = $provinces;
-$provinces_citizen = $QProvince->get_all2(null, ID_PROVINCE_CITIZEN_IDENTITY_CARD);
-$this->view->provinces_citizen = $provinces_citizen;
-
-$QReligion = new Application_Model_Religion();
-$this->view->religions = $QReligion->fetchAll();
-
-$QNationality = new Application_Model_Nationality();
-$this->view->nationalities = $QNationality->fetchAll();
-
+$id                  = $this->getRequest()->getParam('id');
+$QTeam               = new Application_Model_Team();
+$QProvince           = new Application_Model_Province();
+$QReligion           = new Application_Model_Religion();
+$QNationality        = new Application_Model_Nationality();
 $QStaffOffdateReason = new Application_Model_StaffDateoffReason();
+
+if ($id) {
+    $QStaff = new Application_Model_Staff();
+    $staffRowset = $QStaff->find($id);
+    $staff = $staffRowset->current();
+    $this->view->staff = $staff;
+}
+$recursiveDeparmentTeamTitle = $QTeam->get_recursive_cache();
+$provinces = $QProvince->get_all2(null, null, ID_PROVINCE_CITIZEN_IDENTITY_CARD);
+$provinces_citizen = $QProvince->get_all2(null, ID_PROVINCE_CITIZEN_IDENTITY_CARD);
+
+$this->view->recursiveDeparmentTeamTitle = $recursiveDeparmentTeamTitle;
+$this->view->provinces = $provinces;
+$this->view->provinces_citizen = $provinces_citizen;
+$this->view->religions = $QReligion->fetchAll();
+$this->view->nationalities = $QNationality->fetchAll();
 $this->view->staff_dateoff_reason = $QStaffOffdateReason->get_cache();
